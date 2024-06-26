e79b673a1ced26f4511dbc76c1ab662864acd1c1
Peshiroth <sadthapong27@gmail.com>
2024-06-26 11:40:40 +0700

    Assignment 2 Process 1-3 Simple 1-2 Assignment 3 Asyncio1-4 26/6/2024

--- assignment3/asyncio01.py
+++ assignment3/asyncio01.py
@@ -1,1 +1,10 @@
 # check the type of a coroutine
+import asyncio
+#define a coroutine
+async def custom_coro():
+    #wait another coroutine
+    await asyncio.sleep(1)
+#create coroutine
+coro = custom_coro()
+#check the type of the coroutine
+print(type(coro))
